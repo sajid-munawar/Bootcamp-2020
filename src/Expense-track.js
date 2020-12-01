cbdbbd81b741c936bcab10539fc685c857859f99
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-02 00:34:10 +0500

    map mathod on list

--- src/Expense-track.js
+++ src/Expense-track.js
@@ -1,13 +1,36 @@
 import React from 'react'
+
+const transactions= [
+    {amount:500 ,desc:"cash"},
+    {amount:500 ,desc:"cash"},
+    {amount:500 ,desc:"cash"},
+]
+
 function ExpenseTrack() {
     return <div className="app-container">
         <h1>Expense Tacker</h1>
-        <h4>Your Balance <br />$500</h4>
+        <h4 >Your Balance <br />$500</h4>
         <div className="expense-container">
             <h4>Income $700 <br /></h4>
             <h4>Expense $200 <br /></h4>
         </div>
         <h3>History</h3>
+        <ul > {transactions.map((transobj,ind)=>{
+            return <li className="list">
+            <span>{transobj.amount}</span>
+            <span>{transobj.desc}</span>
+        </li>
+        })}
+            
+            <li className="list">
+                <span>Cash</span>
+                <span>200</span>
+            </li>
+            <li className="list">
+                <span>Cash</span>
+                <span>200</span>
+            </li>
+        </ul>
         <hr />
         <form>
             <label className="label">Enter Description <br/> <input type="text"></input></label>
